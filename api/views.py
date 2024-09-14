e10846cf602c09a1adfc204690c09311c5765962
Roygicheru <gicheru.k.roy@gmail.com>
2024-09-14 14:00:38 +0300

    new change

--- api/views.py
+++ api/views.py
@@ -1,15 +1,11 @@
+from django.db.models import Sum
 from rest_framework import viewsets, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
-from django.db.models import Sum
 
-from .models import InvestmentAccount, Transaction
-from .serializers import (
-    InvestmentAccountSerializer,
-    TransactionSerializer,
-    # InvestmentAccountUserSerializer
-)
+from .models import InvestmentAccount, Transaction, InvestmentAccountUser
+from .serializers import InvestmentAccountSerializer, TransactionSerializer
 from .permissions import InvestmentAccountPermission
 
 
@@ -32,17 +28,45 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        account_id = self.kwargs.get(
-            'account_id')
-        queryset = super().get_queryset().filter(
-            investment_account__users=user
-        )
-        if account_id:
-            queryset = queryset.filter(investment_account_id=account_id)
+        account_id = self.kwargs.get('account_id')
 
-        return queryset
+        user_permission = InvestmentAccountUser.objects.filter(
+            user=user,
+            investment_account_id=account_id
+        ).first()
+
+        if not user_permission:
+            return Transaction.objects.none()
+
+        if user_permission.permission in ['crud', 'view']:
+            return Transaction.objects.filter(investment_account_id=account_id)
+        return Transaction.objects.none()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if not queryset.exists():
+            return Response(
+                {"detail": "You don't have permission"
+                 " to view these transactions."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
+        account_id = self.kwargs.get('account_id')
+        user_permission = InvestmentAccountUser.objects.filter(
+            user=request.user,
+            investment_account_id=account_id
+        ).first()
+
+        if not user_permission or user_permission.permission not in ['crud',
+                                                                     'post']:
+            return Response(
+                {"detail": "You can't create transactions for this account."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
         serializer = self.get_serializer(
             data=request.data,
             context={'request': request, 'view': self}
